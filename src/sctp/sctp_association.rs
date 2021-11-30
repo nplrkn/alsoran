65edfad9b5631c0deba2d0b314fb07cf6246b93f
Nic Larkin <nplrkn@gmail.com>
2021-11-30 21:36:35 +0100

    Tidying

--- src/sctp/sctp_association.rs
+++ src/sctp/sctp_association.rs
@@ -24,78 +24,75 @@ impl AsRawFd for SctpAssociation {
     }
 }
 
+// Disable nagling and enable SCTP_RCVINFO on a given socket.
+fn set_sock_opts(fd: i32, logger: &Logger) -> io::Result<()> {
+    // RFC6458, 8.1.29 - This option expects an integer boolean flag, where a non-zero value
+    // turns on the option, and a zero value turns off the option.
+    let enabled = &1 as *const _ as *const _;
+    let enabled_len = std::mem::size_of::<libc::c_int>() as libc::socklen_t;
+    if unsafe {
+        setsockopt(
+            fd,
+            SOL_SCTP as libc::c_int,
+            SCTP_NODELAY as libc::c_int,
+            enabled,
+            enabled_len,
+        )
+    } < 0
+    {
+        warn!(
+            logger,
+            "Failed to set NODELAY socket option - {}",
+            Error::last_os_error()
+        );
+    };
+
+    if unsafe {
+        setsockopt(
+            fd,
+            SOL_SCTP as libc::c_int,
+            SCTP_RECVRCVINFO as libc::c_int,
+            enabled,
+            enabled_len,
+        )
+    } < 0
+    {
+        let e = Error::last_os_error();
+        error!(
+            logger,
+            "Failed to set SCTP_RECVRCVINFO socket option - {}", e
+        );
+        Err(e)
+    } else {
+        Ok(())
+    }
+}
+
 impl SctpAssociation {
-    // See https://docs.rs/async-net/1.6.1/async_net/struct.TcpStream.html
+    // Establish an association as a client
     pub async fn establish<A: AsyncToSocketAddrs>(
         addr: A,
         logger: &Logger,
     ) -> io::Result<SctpAssociation> {
-        // Set up assocation as client
+        // Get a socket
+        let fd = unsafe { socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP) };
+
+        // Set up sock opts
+        set_sock_opts(fd, logger)?;
+
+        // Connect
+        // TODO nonblocking
         let addr = async_net::resolve(addr).await.map(|vec| vec[0])?;
         let addr: OsSocketAddr = addr.into();
-
-        let connected_socket_fd: i32 = unsafe {
-            let s = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
-            // TODO error handling
-
-            // Disable nagling and enable SCTP_RCVINFO.
-            // RFC6458, 8.1.29 - This option expects an integer boolean flag, where a non-zero value
-            // turns on the option, and a zero value turns off the option.
-            let enabled: libc::c_int = 1;
-            let enabled = &enabled as *const _ as *const _;
-            let enabled_len = std::mem::size_of::<libc::c_int>() as libc::socklen_t;
-
-            println!("NODELAY setsockopt {:?}", s);
-            if setsockopt(
-                s,
-                SOL_SCTP as libc::c_int,
-                SCTP_NODELAY as libc::c_int,
-                enabled,
-                enabled_len,
-            ) < 0
-            {
-                warn!(
-                    logger,
-                    "Failed to set NODELAY socket option - {}",
-                    Error::last_os_error()
-                );
-            };
-
-            if setsockopt(
-                s,
-                SOL_SCTP as libc::c_int,
-                SCTP_RECVRCVINFO as libc::c_int,
-                enabled,
-                enabled_len,
-            ) < 0
-            {
-                let e = Error::last_os_error();
-                error!(
-                    logger,
-                    "Failed to set SCTP_RECVRCVINFO socket option - {}", e
-                );
-                Err(e)
-            } else {
-                Ok(())
-            }?;
-
-            // TODO nonblocking connect
-            if connect(s, addr.as_ptr(), addr.len()) < 0 {
-                let e = Error::last_os_error();
-                error!(logger, "Failed SCTP connect to {:?} - {}", addr, e);
-                Err(e)
-            } else {
-                Ok(s)
-            }
+        if unsafe { connect(fd, addr.as_ptr(), addr.len()) } < 0 {
+            let e = Error::last_os_error();
+            error!(logger, "Failed SCTP connect to {:?} - {}", addr, e);
+            Err(e)
+        } else {
+            Ok(())
         }?;
 
-        //let fd = Async::new(connected_socket_fd)?;
-
-        // Put the fd into non-blocking mode.  This ought to be moved before the connect.
-
-        Ok(SctpAssociation {
-            fd: connected_socket_fd,
-        })
+        Ok(SctpAssociation { fd })
     }
 
     // pub async fn send(&self, _buf: &[u8], _stream_id: u32) -> Result<usize> {
