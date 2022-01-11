84c17202b230af3b119873f4cd8474213d48b987
Nic Larkin <nplrkn@gmail.com>
2022-01-11 09:06:48 +0100

    Move call to NG setup

--- worker/src/ngap_handler.rs
+++ worker/src/ngap_handler.rs
@@ -2,11 +2,10 @@ use crate::gnbcu::Gnbcu;
 use crate::{ClientContext, F1ServerTransportProvider, NgapClientTransportProvider};
 use also_net::{TnlaEvent, TnlaEventHandler};
 use async_trait::async_trait;
-use bitvec::vec::BitVec;
 use common::ngap::*;
 use node_control_api::Api;
 use slog::Logger;
-use slog::{info, trace, warn};
+use slog::{trace, warn};
 use std::sync::Arc;
 
 #[derive(Debug, Clone)]
@@ -45,39 +44,7 @@ where
         match event {
             TnlaEvent::Established => {
                 trace!(logger, "TNLA {} established", tnla_id);
-
-                info!(logger, "Send NG setup to AMF");
-                let ng_setup = NgapPdu::InitiatingMessage(InitiatingMessage {
-                    procedure_code: ProcedureCode(21),
-                    criticality: Criticality(Criticality::REJECT),
-                    value: InitiatingMessageValue::IdNgSetup(NgSetupRequest {
-                        protocol_i_es: NgSetupRequestProtocolIEs(vec![
-                            NgSetupRequestProtocolIEsItem {
-                                id: ProtocolIeId(27),
-                                criticality: Criticality(Criticality::REJECT),
-                                value: NgSetupRequestProtocolIEsItemValue::IdGlobalRanNodeId(
-                                    GlobalRanNodeId::GlobalGnbId(GlobalGnbId {
-                                        plmn_identity: PlmnIdentity(vec![2, 3, 2, 1, 5, 6]),
-                                        gnb_id: GnbId::GnbId(BitString26(BitVec::from_element(
-                                            0x10,
-                                        ))),
-                                        ie_extensions: None,
-                                    }),
-                                ),
-                            },
-                        ]),
-                    }),
-                });
-                //let precanned_ng_setup = hex::decode("00150035000004001b00080002f83910000102005240090300667265653567630066001000000000010002f839000010080102030015400140").unwrap();
-                match self
-                    .gnbcu
-                    .ngap_transport_provider
-                    .send_pdu(ng_setup, logger)
-                    .await
-                {
-                    Ok(()) => (),
-                    Err(e) => warn!(logger, "Failed NG Setup send - {:?}", e),
-                }
+                // TODO update coordinator
             }
             TnlaEvent::Terminated => warn!(logger, "TNLA {} closed", tnla_id),
         };
--- worker/src/node_control_callback_server.rs
+++ worker/src/node_control_callback_server.rs
@@ -2,7 +2,10 @@
 use crate::gnbcu::Gnbcu;
 use crate::{ClientContext, F1ServerTransportProvider, NgapClientTransportProvider};
 use async_trait::async_trait;
+use bitvec::vec::BitVec;
+use common::ngap::*;
 use node_control_api::{models, Api, CallbackApi, TriggerInterfaceManagementResponse};
+use slog::{info, warn};
 use swagger::ApiError;
 
 #[async_trait]
@@ -18,17 +21,50 @@ where
         &self,
         _callback_request_body_callback_url: String,
         _tnla_id: i32,
-        _interface_management_req: models::InterfaceManagementReq,
+        interface_management_req: models::InterfaceManagementReq,
         _context: &Cx,
     ) -> Result<TriggerInterfaceManagementResponse, ApiError> {
-        unimplemented!();
-        // let context = context.clone();
-        // info!(
-        //     "trigger_interface_management({}, {:?}) - X-Span-ID: {:?}",
-        //     tnla_id,
-        //     interface_management_req,
-        //     context.get().0.clone()
-        // );
-        //Err("Generic failure".into())
+        let logger = &self.logger;
+
+        // TODO if a worker has max 1 connection to an AMF address, we don't need the TNLA ID.
+        // We just need the AMF endpoint address.  Let's change the above to no longer ref tnla ID.
+
+        // TODO check procedure and send RAN configuration update.
+        info!(logger, "Send NG setup to AMF");
+        let ng_setup = NgapPdu::InitiatingMessage(InitiatingMessage {
+            procedure_code: ProcedureCode(21),
+            criticality: Criticality(Criticality::REJECT),
+            value: InitiatingMessageValue::IdNgSetup(NgSetupRequest {
+                protocol_i_es: NgSetupRequestProtocolIEs(vec![NgSetupRequestProtocolIEsItem {
+                    id: ProtocolIeId(27),
+                    criticality: Criticality(Criticality::REJECT),
+                    value: NgSetupRequestProtocolIEsItemValue::IdGlobalRanNodeId(
+                        GlobalRanNodeId::GlobalGnbId(GlobalGnbId {
+                            plmn_identity: PlmnIdentity(vec![2, 3, 2, 1, 5, 6]),
+                            gnb_id: GnbId::GnbId(BitString26(BitVec::from_element(0x10))),
+                            ie_extensions: None,
+                        }),
+                    ),
+                }]),
+            }),
+        });
+        //let precanned_ng_setup = hex::decode("00150035000004001b00080002f83910000102005240090300667265653567630066001000000000010002f839000010080102030015400140").unwrap();
+        // TODO use tnla_id
+        match self
+            .ngap_transport_provider
+            .send_pdu(ng_setup, logger)
+            .await
+        {
+            Ok(()) => Ok(TriggerInterfaceManagementResponse::InterfaceManagementResponse),
+            Err(e) => {
+                warn!(logger, "Failed NG Setup send - {:?}", e);
+                Ok(TriggerInterfaceManagementResponse::UnexpectedError(
+                    models::Error {
+                        code: 0,
+                        message: format!("Failed {}: {:?}", interface_management_req.procedure, e),
+                    },
+                ))
+            }
+        }
     }
 }
