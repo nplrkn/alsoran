4c3cf904b755569439c0d6174eab67ebf7043682
Nic Larkin <nplrkn@gmail.com>
2022-01-16 09:55:48 +0100

    Add todo

--- integration-tests/tests/second_worker.rs
+++ integration-tests/tests/second_worker.rs
@@ -8,7 +8,7 @@ async fn ran_configuration_update_for_second_worker() -> Result<()> {
     let mut test_context = TestContext::new().await;
 
     test_context.amf.expect_connection().await;
-    test::ng_setup::handle(&test_context).await;
+    test::ng_setup::handle(&test_context).await; // TODO change to amf.expect_connection(), amf.handle_ng_setup
 
     // Start a second worker.
     test_context.start_worker().await;
