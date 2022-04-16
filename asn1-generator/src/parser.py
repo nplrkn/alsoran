564e70f0c34fb7c82c23686645a186c6915b873d
Nic Larkin <nplrkn@gmail.com>
2022-04-16 09:27:41 +0200

    Top PDU generation

--- asn1-generator/src/parser.py
+++ asn1-generator/src/parser.py
@@ -1,8 +1,8 @@
 from lark import Lark
 
 
-def parse_string(s):
-    file = open("grammar.lark")
+def parse_string(s, grammar='grammar.lark'):
+    file = open(grammar)
     grammar = file.read()
     file.close()
     print("---- Parsing ----")
@@ -10,8 +10,8 @@ def parse_string(s):
     return parser.parse(s)
 
 
-def parse_file(f):
+def parse_file(f, grammar='grammar.lark'):
     file = open(f)
     input = file.read()
     file.close()
-    return parse_string(input)
+    return parse_string(input, grammar)
