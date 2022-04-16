b2f964af0d7beb4eb35f80797e3711c5893a0919
Nic Larkin <nplrkn@gmail.com>
2022-04-16 10:48:45 +0200

    Clearing out unused stuff

--- f1ap_old/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// Given we don't have a proper F1AP Rust compiler or PER encoder
-// these are handmade and use serde serialization instead.
-
-mod f1ap_pdu_contents;
-pub use f1ap_pdu_contents::*;
-mod f1ap_ies;
-pub use f1ap_ies::*;
--- ngap_old/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod common_data_types;
-mod constants;
-mod ies;
-mod pdu_contents;
-mod pdu_descriptions;
