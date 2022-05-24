6d8215842f1ce8bd24964453549abc80d5d5bf3e
Nic Larkin <nplrkn@gmail.com>
2022-05-24 08:21:11 +0200

    Fix ambiguous use

--- rrc/src/lib.rs
+++ rrc/src/lib.rs
@@ -1,6 +1,6 @@
 mod rrc;
+pub use crate::rrc::*;
 use asn1_codecs::aper::{AperCodec, AperCodecData, AperCodecError};
-pub use rrc::*;
 
 #[derive(Clone, Debug)]
 pub enum SetupRelease<T: AperCodec> {
