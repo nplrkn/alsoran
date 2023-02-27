aa6fd4502a18b5cc3fda13265f30bb7c8c254d04
Nic Larkin <nplrkn@gmail.com>
2023-02-27 08:38:56 +0100

    Exit on failed bind()

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,3 +2,4 @@ pub mod logging;
 pub mod shutdown_handle;
 pub mod signal;
 pub use shutdown_handle::ShutdownHandle;
+pub mod panic;
---
+++ common/src/panic.rs
@@ -0,0 +1,9 @@
+use std::{panic, process};
+
+pub fn exit_on_panic() {
+    let orig_hook = panic::take_hook();
+    panic::set_hook(Box::new(move |panic_info| {
+        orig_hook(panic_info);
+        process::exit(1);
+    }));
+}
